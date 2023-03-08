c7c0f772f4f74e077005bad60909ff1f123689b5
Tiago Alves <tiago.alves@blip.pt>
2023-03-08 17:18:29 +0000

    update config

--- nvim/lua/alvest/packer.lua
+++ nvim/lua/alvest/packer.lua
@@ -1,17 +1,16 @@
---"; ";This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use("wbthomason/packer.nvim")
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-     -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.1",
+    -- or                            , branch = '0.1.x',
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
   -- use({
   --         'rose-pine/neovim',
@@ -22,86 +21,95 @@ return require('packer').startup(function(use)
   --         end
   -- })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('tpope/vim-fugitive')
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+  use("tpope/vim-fugitive")
+  use("lewis6991/gitsigns.nvim")
 
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v1.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  use({
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },          -- Required
+      { "williamboman/mason.nvim" },        -- Optional
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },      -- Required
+      { "hrsh7th/cmp-nvim-lsp" },  -- Required
+      { "hrsh7th/cmp-buffer" },    -- Optional
+      { "hrsh7th/cmp-path" },      -- Optional
+      { "saadparwaiz1/cmp_luasnip" }, -- Optional
+      { "hrsh7th/cmp-nvim-lua" },  -- Optional
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
-  }
-}
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },          -- Required
+      { "rafamadriz/friendly-snippets" }, -- Optional
 
-use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'}
--- Lua
-use {
+      -- Formatting
+      { "neovim/nvim-lspconfig" },
+      { "jose-elias-alvarez/null-ls.nvim" },
+    },
+  })
+
+  use({ "akinsho/flutter-tools.nvim", requires = "nvim-lua/plenary.nvim" })
+  -- Lua
+  use({
     "folke/trouble.nvim",
     requires = "nvim-tree/nvim-web-devicons",
     config = function()
-        require("trouble").setup {
-            icons = false,
-            fold_open = "v", -- icon used for open folds
-            fold_closed = ">", -- icon used for closed folds
-            indent_lines = false, -- add an indent guide below the fold icons
-            signs = {
-                -- icons / text used for a diagnostic
-                error = "error",
-                warning = "warn",
-                hint = "hint",
-                information = "info"
-            },
-            use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    end
-}
+      require("trouble").setup({
+        icons = false,
+        fold_open = "v",  -- icon used for open folds
+        fold_closed = ">", -- icon used for closed folds
+        indent_lines = false, -- add an indent guide below the fold icons
+        signs = {
+          -- icons / text used for a diagnostic
+          error = "error",
+          warning = "warn",
+          hint = "hint",
+          information = "info",
+        },
+        use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  })
 
-use('christoomey/vim-tmux-navigator')
-use('numToStr/Comment.nvim')
-use('nvim-tree/nvim-tree.lua')
+  use("christoomey/vim-tmux-navigator")
+  use("numToStr/Comment.nvim")
+  use("nvim-tree/nvim-tree.lua")
 
-use {
+  use({
     "ggandor/leap.nvim",
-    config = function() require("leap").set_default_keymaps() end
-}
+    config = function()
+      require("leap").set_default_keymaps()
+    end,
+  })
 
-use { "catppuccin/nvim", as = "catppuccin" }
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-}
-use "rafamadriz/friendly-snippets"
+  use({ "catppuccin/nvim", as = "catppuccin" })
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
+  use("rafamadriz/friendly-snippets")
 
-use 'nvim-tree/nvim-web-devicons'
-use({
-  "utilyre/barbecue.nvim",
-  tag = "*",
-  requires = {
-    "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
-  },
-    after = "nvim-web-devicons", -- keep this if you're using NvChad
-  config = function()
-    require("barbecue").setup()
-  end,
-})
-
+  use("nvim-tree/nvim-web-devicons")
+  use({
+    "utilyre/barbecue.nvim",
+    tag = "*",
+    requires = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    after = "nvim-web-devicons",  -- keep this if you're using NvChad
+    config = function()
+      require("barbecue").setup()
+    end,
+  })
+  use("kkharji/sqlite.lua")
+  use({ "smartpde/telescope-recent-files" })
 end)
--- nvim/lua/alvest/remap.lua
+++ nvim/lua/alvest/remap.lua
@@ -16,23 +16,36 @@ vim.keymap.set("n", "<leader>+", "<C-a>") -- increment
 vim.keymap.set("n", "<leader>-", "<C-x>") -- decrement
 
 -- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+vim.keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s")        -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=")        -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>")    -- close current split window
 
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
 vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
 
 -- nvim-tree
 local tree_api = require("nvim-tree.api")
 
 vim.keymap.set("n", "<leader>b", function()
-    tree_api.tree.toggle({
-        find_file = true
-    })
-    -- print(tree)
+  tree_api.tree.toggle({
+    find_file = true,
+  })
+  -- print(tree)
 end)
 
+-- recent files
+-- This may bind to old telescope function depending on your load order:
+-- vim.keymap.set('n', '<leader>f', require'telescope'.builtins.find_files)
+-- So: better wrap it in a function:
+vim.keymap.set("n", "<leader>f", function()
+  require("telescope").builtins.find_files()
+end)
+-- vim.api.nvim_set_keymap(
+--   "n",
+--   "<leader><leader>",
+--   "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>",
+--   { noremap = true, silent = true }
+-- )
