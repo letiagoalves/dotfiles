f72b1b5013edb3d1e5e45f112059c751f502afdf
Tiago Alves <tiago.alves@blip.pt>
2025-01-15 16:17:48 +0000

    add fish and ghostty config files

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
+set fish_greeting
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # testing startup time
+    # fish --profile-startup /tmp/fish.profile -i -c exit
+
+    fish_add_path "$HOME/homebrew/bin"
+    fish_add_path "$HOME/workspace/github/git-fuzzy/bin"
+    fish_add_path "$HOME/.cargo/bin"
+    fish_add_path "$HOME/.local/share/bob/nvim-bin"
+
+    starship init fish | source
+    zoxide init fish | source
 end
+
