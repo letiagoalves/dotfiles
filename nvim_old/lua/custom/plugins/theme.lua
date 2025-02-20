2e0780aef67b1a77702a56263f4166e229ad322d
Tiago Alves <tiago.alves@blip.pt>
2025-02-20 16:11:36 +0000

    try out lazy vim

---
+++ nvim_old/lua/custom/plugins/theme.lua
@@ -0,0 +1,68 @@
+return {
+  {
+    "catppuccin/nvim",
+    tag = "v1.1.0",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+        term_colors = true,
+        dim_inactive = {
+          enabled = true,
+          shade = "dark",
+          percentage = 0.1,
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false,   -- Force no bold
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {
+          -- all = {
+          --   text = "#ff0000",
+          -- },
+          -- mocha = {
+          --   base = "#222222",
+          --   mantle = "#191919",
+          --   crust = "#474747",
+          -- },
+          -- mocha = {
+          --   base = "#161622",
+          --   mantle = "#000000",
+          --   crust = "#CFFDE1",
+          -- },
+        },
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          notify = false,
+          mini = false,
+          leap = true,
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end
+  }
+}
---
+++ nvim_old/lua/custom/plugins/treesitter.lua
@@ -0,0 +1,7 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        make = ":TSUpdate",
+        name = "nvim-treesitter",
+    }
+}
